d5a452ee11e488dfab12d81522bd19712dc0a505
mstewart796 <m.stewart796@gmail.com>
2024-01-24 12:44:32 -0300

    Naive bayes 0.786 accuracy

---
+++ Titanic/naive_bayes_classification.py
@@ -0,0 +1,48 @@
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('titanic_train.csv')
+X = dataset.iloc[:, [2, 4, 5]].values
+y = dataset.iloc[:, [1]].values
+
+df_test = pd.read_csv('titanic_test.csv')
+X_test = df_test.iloc[:, [1, 3, 4]].values
+
+# Taking care of missing data
+from sklearn.impute import SimpleImputer
+imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
+imputer.fit(X[:, [2]])
+X[:, [2]] = imputer.transform(X[:, [2]])
+
+imputer_test = SimpleImputer(missing_values=np.nan, strategy='mean')
+imputer_test.fit(X_test[:, [2]])
+X_test[:, [2]] = imputer.transform(X_test[:, [2]])
+
+# Encoding categorical data
+# Encoding the Independent Variable
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [1])], remainder='passthrough')
+X = np.array(ct.fit_transform(X))
+
+ct_test = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [1])], remainder='passthrough')
+X_test = np.array(ct_test.fit_transform(X_test))
+
+# Training the Naive Bayes Classification model on the Training set
+from sklearn.naive_bayes import GaussianNB
+# Flatten the 'y' array using ravel()
+y_flattened = y.ravel()
+
+# Create and train the GaussianNB Classifier
+classifier = GaussianNB()
+classifier.fit(X, y_flattened)
+
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
+
+# Checking accuracy
+train_accuracy = classifier.score(X, y)
+print(f'Training Accuracy: {train_accuracy}')
