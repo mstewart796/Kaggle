f58f12d4a0f4cfa5f8e3289cb1dbd132740a928e
mstewart796 <m.stewart796@gmail.com>
2024-01-31 14:51:15 -0300

    Logistic Regression Version 2 - Kaggle 0.75598 accuracy

---
+++ Titanic/logistic_reg_model_ver_2.py
@@ -0,0 +1,48 @@
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('titanic_train.csv')
+X = dataset.iloc[:, [2, 4, 5, 6, 7, 9]].values
+y = dataset.iloc[:, 1].values
+
+df_test = pd.read_csv('titanic_test.csv')
+X_test = df_test.iloc[:, [1, 3, 4, 5, 6, 8]].values
+
+# Taking care of missing data
+from sklearn.impute import SimpleImputer
+imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
+imputer.fit(X[:, [2, 5]])
+X[:, [2, 5]] = imputer.transform(X[:, [2, 5]])
+
+imputer_test = SimpleImputer(missing_values=np.nan, strategy='mean')
+imputer_test.fit(X_test[:, [2, 5]])
+X_test[:, [2, 5]] = imputer.transform(X_test[:, [2, 5]])
+
+# Encoding categorical data
+# Encoding the Independent Variable
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [1])], remainder='passthrough')
+X = np.array(ct.fit_transform(X))
+
+ct_test = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [1])], remainder='passthrough')
+X_test = np.array(ct_test.fit_transform(X_test))
+
+# Training the Logistic Regression model on the Training set
+from sklearn.linear_model import LogisticRegression
+# Flatten the 'y' array using ravel()
+y_flattened = y.ravel()
+
+# Create and train the DecisionTreeClassifier
+classifier = LogisticRegression(max_iter=1000)
+classifier.fit(X, y_flattened)
+
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
+
+# Checking accuracy
+train_accuracy = classifier.score(X, y)
+print(f'Training Accuracy: {train_accuracy}')
