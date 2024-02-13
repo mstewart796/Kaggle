86267116539672a8ed5f1d3a3223abf04cf92feb
mstewart796 <m.stewart796@gmail.com>
2024-02-13 17:33:17 -0300

    Added feature scaling, validation and confusion matrix to logistic regression model

--- Titanic/logistic_reg_model_ver_2.py
+++ Titanic/logistic_reg_model_ver_2.py
@@ -5,21 +5,21 @@ import pandas as pd
 
 # Importing the dataset
 dataset = pd.read_csv('titanic_train.csv')
-X = dataset.iloc[:, [2, 4, 5, 6, 7, 9]].values
+X = dataset.iloc[:, [2, 4, 5]].values
 y = dataset.iloc[:, 1].values
 
 df_test = pd.read_csv('titanic_test.csv')
-X_test = df_test.iloc[:, [1, 3, 4, 5, 6, 8]].values
+X_test = df_test.iloc[:, [1, 3, 4]].values
 
 # Taking care of missing data
 from sklearn.impute import SimpleImputer
 imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
-imputer.fit(X[:, [2, 5]])
-X[:, [2, 5]] = imputer.transform(X[:, [2, 5]])
+imputer.fit(X[:, [2]])
+X[:, [2]] = imputer.transform(X[:, [2]])
 
 imputer_test = SimpleImputer(missing_values=np.nan, strategy='mean')
-imputer_test.fit(X_test[:, [2, 5]])
-X_test[:, [2, 5]] = imputer.transform(X_test[:, [2, 5]])
+imputer_test.fit(X_test[:, [2]])
+X_test[:, [2]] = imputer.transform(X_test[:, [2]])
 
 # Encoding categorical data
 # Encoding the Independent Variable
@@ -28,18 +28,32 @@ le = LabelEncoder()
 X[:, 1] = le.fit_transform(X[:, 1])
 X_test[:, 1] = le.fit_transform(X_test[:, 1])
 
+# Split into training and validation sets
+from sklearn.model_selection import train_test_split
+X_train, X_valid, y_train, y_valid = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_valid = sc.transform(X_valid)
+
 # Training the Logistic Regression model on the Training set
 from sklearn.linear_model import LogisticRegression
 # Flatten the 'y' array using ravel()
-y_flattened = y.ravel()
+y_flattened = y_train.ravel()
 
-# Create and train the DecisionTreeClassifier
+# Create and train the Logistic Regression Classifier
 classifier = LogisticRegression(max_iter=1000)
-classifier.fit(X, y_flattened)
+classifier.fit(X_train, y_flattened)
 
-# Predicting the Test set results
-y_pred = classifier.predict(X_test)
+# Predicting the Validation set results
+y_pred = classifier.predict(X_valid)
 
 # Checking accuracy
-train_accuracy = classifier.score(X, y)
-print(f'Training Accuracy: {train_accuracy}')
+
+from sklearn.metrics import confusion_matrix, accuracy_score
+y_pred = classifier.predict(X_valid)
+cm = confusion_matrix(y_valid, y_pred)
+print(cm)
+print(accuracy_score(y_valid, y_pred))
